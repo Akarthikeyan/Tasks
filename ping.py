acd141ad39929927306eeb4ff697d4d98fcf6335
Akarthikeyan <akarthikeyancse@gmail.com>
2014-01-31 17:49:57 +0530

    Create ping.py
    to use the another network from one network

---
+++ ping.py
@@ -0,0 +1,11 @@
+import tkinter #for version 3.0, if version before 3.0 use import Tkinter
+import os,sys
+ak=tk()
+def ping2():
+  os.system('ping1')
+a=Button(pen)
+ip=("192.168.0.1")
+a.config(text="PING",bg="white",fg="red")
+a=ping2.ip
+a.pack()
+ak.mainloop()
