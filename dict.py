4721d3863900cd200b0c74a724ecd57dd20068c3
Akarthikeyan <akarthikeyancse@gmail.com>
2014-01-31 21:15:47 +0530

    Create dict.py
    For using the dictionary type operations

---
+++ dict.py
@@ -0,0 +1,5 @@
+dict={'abc': 1234, 'def': 5678}
+dict['ghijk']=2468
+del dict['def']                 #To delete the particular record
+dict.keys()                     #For displaying the key names of dictionary
+'ghijk' in dict                 #To detect the word is that present in dictionary
